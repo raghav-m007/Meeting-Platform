7ecde4d5dd157d56d972edd091ae36ef2064a454
raghav-m007 <raghav.mittal@iitg.ac.in>
2022-06-17 23:34:06 +0530

    established video and audio connection

--- server.js
+++ server.js
@@ -2,13 +2,40 @@ const express = require("express");
 const path = require("path");
 var app = express();
 var server = app.listen(3000, function () {
-    console.log("Listening on port 3000");
+  console.log("Listening on port 3000");
 });
 const io = require("socket.io")(server, {
-    allowEIO3: true, // false by default
+  allowEIO3: true, // false by default
 });
 app.use(express.static(path.join(__dirname, "")));
 var userConnections = [];
 io.on("connection", (socket) => {
-    console.log("socket id is ", socket.id);
+  console.log("socket id is ", socket.id);
+
+  socket.on("userconnect", (data) => {
+    console.log("userconnent", data.displayName, data.meetingid);
+    var other_users = userConnections.filter(
+      (p) => p.meeting_id == data.meetingid
+    );
+    userConnections.push({
+      connectionId: socket.id,
+      user_id: data.displayName,
+      meeting_id: data.meetingid,
+    });
+    var userCount = userConnections.length;
+    console.log(userCount);
+    other_users.forEach((v) => {
+      socket.to(v.connectionId).emit("inform_others_about_me", {
+        other_user_id: data.displayName,
+        connId: socket.id,
+      });
+    });
+    socket.emit("inform_me_about_other_user", other_users);
+  });
+  socket.on("SDPProcess", (data) => {
+    socket.to(data.to_connid).emit("SDPProcess", {
+      message: data.message,
+      from_connid: socket.id,
+    });
+  });
 });
